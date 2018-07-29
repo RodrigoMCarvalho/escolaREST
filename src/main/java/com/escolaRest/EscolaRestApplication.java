7fd922a42dc883e5a4209f3224871e580c0704c3
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2018-07-29 20:16:06 -0300

    iniciando projeto

---
+++ src/main/java/com/escolaRest/EscolaRestApplication.java
@@ -0,0 +1,12 @@
+package com.escolaRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscolaRestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/escolaRest/escolaRest/EscolaRestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.escolaRest.escolaRest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EscolaRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
